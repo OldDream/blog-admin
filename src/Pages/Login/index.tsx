b1456dc02a282808c83e4aec7e6b7e28ebc80401
OldDream <huangyn15@qq.com>
2020-02-25 17:40:25 +0800

    add:  添加登录页面

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Card, Input, Icon, Spin } from 'antd';
+import './login.scss'
 
-export default function Login() {
+function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleUsernameInput(e: any) {
+    setUserName(e.target.value)
+    console.log(e.target.value);
+  }
+  function handlePasswordInput(e: any) {
+    setPassword(e.target.value)
+    console.log(e.target.value);
+  }
+  function checkLogin(e: any) {
+    console.log({userName,password});
+    setIsLoading(true)
+    setTimeout(() => {
+    setIsLoading(false)
+    }, 3000);
+  }
+
   return (
-    <div>
-      Login Page
+    <div className="login-div">
+      <Spin tip="Loading..." spinning={isLoading} wrapperClassName="spin-div">
+        <Card title="HYN's Blog Admin Page" bordered={true} className="login-card">
+          <Input
+            // onChange={(e)=> {console.log(e.target.value);}}
+            onChange={handleUsernameInput}
+            id="userName"
+            size="large"
+            value={userName}
+            placeholder="请输入用户名"
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          />
+          <br />
+          <br />
+          <Input.Password
+            onChange={handlePasswordInput}
+            id="password"
+            size="large"
+            value={password}
+            placeholder="请输入密码"
+            prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          />
+          <br /><br />
+          <Button type="primary" size="large" block onClick={checkLogin}>
+            提交
+          </Button>
+        </Card>
+      </Spin>
     </div>
-  )
+  );
 }
+
+export default Login;
