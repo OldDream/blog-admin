e4cc26194c5f916a825e0c8cc957f5148d5a73b3
OldDream <huangyn15@qq.com>
2020-02-28 15:19:02 +0800

    add: 删除文章

--- src/Pages/ArticleList/index.tsx
+++ src/Pages/ArticleList/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, List, Button, Modal } from 'antd';
+import { Row, Col, List, Button, Modal, message } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 import axios from '../../utils/axios';
-import './articleList.scss'
+import './articleList.scss';
 const { confirm } = Modal;
 
 function ArticleList(props: any) {
@@ -15,14 +16,37 @@ function ArticleList(props: any) {
   const [list, setList] = useState<ArticleItem[]>([]);
 
   const getList = () => {
-    axios.get('/admin/getArticleList').then((res) => {
-      setList(res.data.data)
-    })
-  }
+    axios.get('/admin/getArticleList').then(res => {
+      setList(res.data.data);
+    });
+  };
+
+  const deleteArticle = (id: number, index: number) => {
+    console.log(index);
+    confirm({
+      title: '确定删除本文章?',
+      icon: <ExclamationCircleOutlined />,
+      onOk() {
+        console.log('OK');
+        axios.delete('/admin/deleteArticle/' + id).then(res => {
+          console.log(res);
+          if (res.data.success) {
+            message.success('删除成功！');
+            const tempList= [...list]
+            tempList.splice(index, 1)
+            setList(tempList)
+          }
+        });
+      },
+      onCancel() {
+        console.log('Cancel');
+      }
+    });
+  };
 
   useEffect(() => {
     console.log('useEffect in ArticleList');
-    getList()
+    getList();
   }, [props]);
 
   // useEffect(() => {
@@ -57,7 +81,7 @@ function ArticleList(props: any) {
         }
         bordered
         dataSource={list}
-        renderItem={item => (
+        renderItem={(item, index) => (
           <List.Item>
             <Row className="list-div">
               <Col span={1}>{item.id}</Col>
@@ -67,7 +91,14 @@ function ArticleList(props: any) {
               <Col span={3}>{item.view_count}</Col>
               <Col span={4}>
                 <Button type="primary">修改</Button>&nbsp;
-                <Button type="danger">删除 </Button>
+                <Button
+                  type="danger"
+                  onClick={() => {
+                    deleteArticle(item.id, index);
+                  }}
+                >
+                  删除{' '}
+                </Button>
               </Col>
             </Row>
           </List.Item>
